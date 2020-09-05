9e3fcafb9cefe9745aed79e45caa012c95538cec
Frank Pagan <frank@cocreate.app>
2020-09-05 19:53:07 +0000

    fix dndInit

--- demos/dndInit.js
+++ demos/dndInit.js
@@ -1,4 +1,4 @@
 
-window.initDnd({ target: document, droppable: '#dnd', draggable: '#sortid > *' , cloneable: '#cloneable > *' })
+window.initDnd({ target: document, droppable: '#dnd', draggable: '#dnd > *' , cloneable: '#cloneable > *' })
 window.initSortable({ target: document.getElementById('sortable'), handle: '.handle'  })
 
