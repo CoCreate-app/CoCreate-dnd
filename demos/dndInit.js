cb8f1261ad476d3cdeb94c0b52d100350f7f5af8
Frank Pagan <frank@cocreate.app>
2020-09-05 21:47:36 +0000

    update demo

--- demos/dndInit.js
+++ demos/dndInit.js
@@ -1,4 +1,15 @@
-
-window.initDnd({ target: document, drop: '#dnd', drag: '#dnd > *' , clone: '#cloneable > *' })
-window.initSortable({ target: document.getElementById('sortable'), handle: '.handle'  })
+window.initDnd({
+  target: document,
+  drop: "#dnd",
+  drag: "#dnd > *",
+  clone: "#cloneable > *",
+});
+window.initSortable({
+  target: document.getElementById("sortable"),
+  handle: ".handle",
+});
+window.initSortable({
+  target: document.getElementById("sortable1"),
+  handle: ".handle1",
+});
 
