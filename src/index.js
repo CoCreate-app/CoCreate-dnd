5328e6464f78bb2445c3d0bafe8b6363063a15ba
hossein <hossien.alipour@gmail.com>
2020-09-20 00:17:31 +0300

    dfsdfsd

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ import {
 } from "./util/variables.js";
 
 let ref = { x: 0, y: 0, window, document, isIframe: false };
-let zz = 343244442333;
+let zz = 34324444233334343;
 export default function dnd(window, document, options) {
   console.log("dnd is loading", window.location.pathname);
 
