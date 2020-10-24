0e668395b3b492e97e70a7f8ca1ef33b9cfe00ff
jean mendoza <janco1187@gmail.com>
2020-10-24 22:19:37 +0000

    update dnd-config

--- src/dnd-config.js
+++ src/dnd-config.js
@@ -1,15 +1,15 @@
-/*global initSortable*/
+/*global initContainer*/
 
 export default function dndConfigs() {
   let sortables = document.querySelectorAll(".sortable");
 
   sortables.forEach((sortable) => {
-    initSortable({ target: sortable });
+    initContainer({ target: sortable });
   });
 
   let cloneables = document.querySelectorAll(".cloneable");
 
   cloneables.forEach((cloneable) => {
-    initSortable({ target: cloneable, cloneable: true });
+    initContainer({ target: cloneable, cloneable: true });
   });
 }
