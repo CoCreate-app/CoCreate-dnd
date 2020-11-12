2e371945b59e4fb26417f6356b778f4cc8fca3fe
jean mendoza <janco1187@gmail.com>
2020-11-12 20:59:26 +0000

    fix

--- src/virtualDnd.js
+++ src/virtualDnd.js
@@ -56,7 +56,7 @@ export default function virtualDnd(beforeDndSuccess) {
     try {
       if (this.position) {
         if (this.dropedEl === this.dragedEl)
-          throw "dnd cancelled. you can dnd on the same element.";
+          throw "dnd cancelled. you can't dnd on the same element.";
 
         // in future we should also disable hover and tag name in dragOver method
         // parent can't be draged into children
