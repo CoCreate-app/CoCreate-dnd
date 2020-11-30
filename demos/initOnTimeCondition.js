1af8781b4f5f255b82daed3e49a6db4a3293e3a9
jean mendoza <janco1187@gmail.com>
2020-11-30 21:49:36 +0000

    update demos

--- demos/initOnTimeCondition.js
+++ demos/initOnTimeCondition.js
@@ -1,4 +1,5 @@
-  window.initFunction({
+window.CoCreateDnd.initIframe({ document, window });
+window.CoCreateDnd.initFunction({
     target: document.getElementById('onConditionDnd'),
      onDnd:(element, request)=>{    
        return [element, request[0]]
