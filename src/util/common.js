5640d411d568b613470bad7074b45cb186a2a68e
jean mendoza <janco1187@gmail.com>
2020-12-01 00:29:28 +0000

    fix initFunction logic

--- src/util/common.js
+++ src/util/common.js
@@ -1,7 +1,6 @@
 import { group_name } from "./variables";
-import domContext from "./domContext2";
+import domContext, { initFunctionState } from "./domContext2";
 
-let initFunctionState = [];
 //find the global context
 let parentWindow = window;
 while (parentWindow !== window.parent) parentWindow = window.parent;
@@ -13,31 +12,14 @@ if (!parentWindow.dndContext) {
 
 export { dndContext, initFunctionState };
 
-function checkInitFunction(element, request) {
-  for (let state of initFunctionState) {
-    let r = state.onDnd(element, request);
-    if (state.target.contains(element) && Array.isArray(r)) return r;
-  }
-}
-
 export function getCoc(el, att) {
   if (!el.tagName) el = el.parentElement;
-  let r = dndContext.getContext(el, att);
-  if (!r) {
-    let r2 = checkInitFunction(el, [att]);
-    if (Array.isArray(r2) && att == r2[1]) return r2[0];
-    else return;
-  } else return r;
+  return dndContext.getContext(el, att);
 }
 
 export function getCocs(el, attList) {
   if (!el.tagName) el = el.parentElement;
-  let r = dndContext.getContexts(el, attList);
-  if (!Array.isArray(r)) {
-    let r2 = checkInitFunction(el, attList);
-    if (Array.isArray(r2) && attList.includes(r2[1])) return r2;
-    else return;
-  } else return r;
+  return dndContext.getContexts(el, attList);
 }
 
 export function getGroupName(el) {
@@ -336,7 +318,10 @@ export function distanceToChildLeftRight(p, child) {
   let rect = child.getBoundingClientRect();
 
   let line1 = { p1: [rect.top, 0], p2: [rect.bottom, 0] };
-  let line3 = { p1: [rect.top, child.clientWidth], p2: [rect.bottom, child.clientWidth] };
+  let line3 = {
+    p1: [rect.top, child.clientWidth],
+    p2: [rect.bottom, child.clientWidth],
+  };
 
   let distances = [
     pDistance(p[0], p[1], line1.p1[1], line1.p1[0], line1.p2[1], line1.p2[0]),
@@ -359,7 +344,10 @@ export function distanceToChildTopBottom(p, child) {
 
   let line2 = { p1: [0, rect.left], p2: [0, rect.right] };
 
-  let line4 = { p1: [child.clientHeight, rect.left], p2: [child.clientHeight, rect.right] };
+  let line4 = {
+    p1: [child.clientHeight, rect.left],
+    p2: [child.clientHeight, rect.right],
+  };
 
   let distances = [
     pDistance(p[0], p[1], line2.p1[1], line2.p1[0], line2.p2[1], line2.p2[0]),
@@ -390,18 +378,16 @@ export function autoScroller({ speed, threshold }) {
   };
 
   this.calculateScroll = function ({ x, y, element, onMouseScrollMove }) {
-
     while (true) {
       if (
-        element&&
+        element &&
         element.tagName &&
         (element.scrollHeight > element.clientHeight ||
           element.scrollWidth > element.clientWidth)
       ) {
         break;
-      } else if(element.parentElement) element = element.parentElement;
-      else
-       break;
+      } else if (element.parentElement) element = element.parentElement;
+      else break;
     }
 
     let hasHorizontalScrollbar = element.scrollWidth > element.clientWidth;
@@ -427,19 +413,17 @@ export function autoScroller({ speed, threshold }) {
       switch (orientation) {
         case "top":
         case "bottom":
-        
           this.speed = (verScrollThreshold / (closestDistance + 1)) * speed;
           condition = closestDistance < verScrollThreshold;
           break;
 
         case "left":
         case "right":
-  
           this.speed = (horScrollThreshold / (closestDistance + 1)) * speed;
           condition = closestDistance < horScrollThreshold;
           break;
       }
-        
+
       if (condition) {
         if (!this.isActive) {
           this.isActive = true;
@@ -449,7 +433,7 @@ export function autoScroller({ speed, threshold }) {
           this.__activateScroll(element, orientation, onMouseScrollMove);
         }
       } else if (this.isActive) {
-        console.log('no' ,orientation  , element)
+        console.log("no", orientation, element);
         this.isActive = false;
         this.deactivateScroll(element);
       }
@@ -458,17 +442,16 @@ export function autoScroller({ speed, threshold }) {
   // this.__scroll = function(element, s1 = 0, s2 = 0)
   // {
   //   for(let i = 0 ; i <= this.speed; i++)
-  //   { 
+  //   {
   //     element.scrollBy(s1, s2)
-      
+
   //   }
   // }
-  
+
   this.__activateScroll = function (element, orientation, callback) {
-    
     // todo: when interval timeout is low and speed is hight scrollBy doesn't act
     // retodo: give time to scrollBy equal to speed * 0.25 * timeout
-    element.style.scrollBehavior =  'auto';
+    element.style.scrollBehavior = "auto";
     this.lastScrollingElement = element;
     this.interval = setInterval(() => {
       switch (orientation) {
@@ -488,10 +471,9 @@ export function autoScroller({ speed, threshold }) {
           element.scrollBy(this.speed, 0);
 
           break;
-          default:
-        
+        default:
       }
- 
+
       // this.onElement = document.elementFromPoint(this.mouse.x, this.mouse.y);
       // if (this.onElement) {
       //   callback({ x: this.mouse.x, y: this.mouse.y, target: this.onElement });
@@ -500,8 +482,7 @@ export function autoScroller({ speed, threshold }) {
   };
 
   this.deactivateScroll = function (element) {
-    if(element)
-      element.style.scrollBehavior =  '';
+    if (element) element.style.scrollBehavior = "";
     clearInterval(this.interval);
   };
 }
--- src/util/domContext2.js
+++ src/util/domContext2.js
@@ -1,7 +1,18 @@
 // todo: jest support import/export
+
+let initFunctionState = [];
+export { initFunctionState };
 export default function context(html) {
   this.map = new Map();
 
+  this.checkInitFunction = function checkInitFunction(element, request) {
+    for (let state of initFunctionState) {
+      if (state.target.contains(element)) {
+        let r = state.onDnd(element, request);
+        if (Array.isArray(r)) return r;
+      }
+    }
+  };
   /**
    * @param el the element look for context
    *
@@ -10,6 +21,9 @@ export default function context(html) {
   this.getContext = function (el, attributeName) {
     do {
       if (el.getAnyAttribute(attributeName) == "true") return el;
+      else {
+        if (this.checkInitFunction(el, [attributeName])) return el;
+      }
       el = el.parentElement;
     } while (el);
 
@@ -24,6 +38,11 @@ export default function context(html) {
       for (let attributeName of attributeNames) {
         if (el.getAnyAttribute(attributeName) == "true")
           return [el, attributeName];
+        else {
+          let r2 = this.checkInitFunction(el, attributeNames);
+          if (Array.isArray(r2) && attributeNames.includes(r2[1])) return r2;
+          else return;
+        }
       }
 
       el = el.parentElement;
