556b121ae57bbf6d206bafc885076d7a94367ca2
Frank <frankpagan>
2021-04-06 17:12:55 +0000

    add logic for .sortable and .cloneable on load

--- src/dnd-config.js
+++ src/dnd-config.js
@@ -9,25 +9,32 @@ export default function dndConfigs() {
   observer.init({
     name: "dnd-config",
     observe: ["childList"],
-    include:'.sortable, .cloneable',
+    include: '.sortable, .cloneable',
     callback: mutation => {
-      if(mutation.target.classList.has('sortable'))
-         initContainer({ target: mutation.target });
-        else
-         initContainer({ target: mutation.target, cloneable: true });
-      // let sortables = document.querySelectorAll(".sortable");
-    
-      // sortables.forEach((sortable) => {
-      //   initContainer({ target: sortable });
-      // });
-    
-      // let cloneables = document.querySelectorAll(".cloneable");
-    
-      // cloneables.forEach((cloneable) => {
-      //   initContainer({ target: cloneable, cloneable: true });
-      // });
+      console.log('dnd config ', mutation, mutation.target)
+      if (mutation.target.classList.contains('sortable'))
+        initContainer({ target: mutation.target });
+      else if (mutation.target.classList.contains('cloneable'))
+        initContainer({ target: mutation.target, cloneable: true });
+
     },
   });
+
+
+  let sortables = document.querySelectorAll(".sortable");
+
+  sortables.forEach((sortable) => {
+    console.log('dnd config sortable', sortable)
+    initContainer({ target: sortable });
+  });
+
+  let cloneables = document.querySelectorAll(".cloneable");
+
+  cloneables.forEach((cloneable) => {
+    console.log('dnd config cloneable',cloneable)
+    initContainer({ target: cloneable, cloneable: true });
+  });
+
 
 
 }
