87f5dcb6b769274080c045b76f19fafa4b5052d7
frank <frank@cocreate.app>
2021-06-02 23:34:29 +0000

    fix dnd-config

--- src/dnd-config.js
+++ src/dnd-config.js
@@ -9,35 +9,33 @@ export default function dndConfigs() {
   observer.init({
     name: "dnd-config",
     observe: ["childList"],
-    	include: ".sortable, .cloneable",
+    include: ".sortable, .cloneable",
     callback: mutation => {
-       console.log('dnd domReader mutation ', new Date().getSeconds()  + ' ' + new Date().getMilliseconds())
-      init( mutation.target.parentElement.parentElement)
-      // console.log('dnd config observer', mutation.target, mutation.target.parentElement, mutation)
+      let el = mutation.target?.parentElement || mutation.target;
+
+      if (el)
+        init(el)
     },
   });
 
 
 
-init(document)
+  init(document)
 
 
 }
 
 
-function init(el){
-   console.log('dnd domReader init ', new Date().getSeconds()  + ' ' + new Date().getMilliseconds())
+function init(el) {
+
   let sortables = el.querySelectorAll(".sortable");
-  
+
   sortables.forEach((sortable) => {
-    console.log('dnd config sortable', sortable)
     initContainer({ target: sortable });
   });
 
   let cloneables = el.querySelectorAll(".cloneable");
-
   cloneables.forEach((cloneable) => {
-    console.log('dnd config cloneable',cloneable)
     initContainer({ target: cloneable, cloneable: true });
   });
 
