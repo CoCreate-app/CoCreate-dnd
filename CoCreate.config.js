9980981f20ee7d441af171e4a5895cc516af9ff5
frank pagan <frank@cocreate.app>
2021-09-14 03:55:39 +0000

    fix: add cocreate.app to domains

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -14,6 +14,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/dnd/index.html",
                 "domains": [
+                    "cocreate.app",
                     "general.cocreate.app"
                 ],
                 "directory": "/docs/dnd",
