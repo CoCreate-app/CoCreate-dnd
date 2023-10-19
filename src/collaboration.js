128997d88b21558da6612b14b6fc9ce4dd761b34
frankpagan <frank@cocreate.app>
2023-10-19 12:04:18 -0600

    fix: removed @cocreate/message-client

--- src/collaboration.js
+++ src/collaboration.js
@@ -1,69 +1,69 @@
-import {cssPath, parseTextToHtml} from '@cocreate/utils';
-import message from '@cocreate/message-client';
+import { cssPath, parseTextToHtml } from '@cocreate/utils';
+import socket from '@cocreate/socket-client';
 
 function wrapper() {
 
-	if (window.parent !== window) return;
+    if (window.parent !== window) return;
 
-	window.addEventListener("load", () => {
-		message.listen('dnd', function(response) {
-			try {
-				let data = response.data;
-				if (data.path) {
-					let iframe = document.querySelector(data.path);
-					let frame = iframe.contentWindow.document || iframe.contentDocument;
-					data.target = frame.querySelector(data.target);
-				} else {
-					data.target = document.querySelector(data.target);
-				}
-				if (!data.target)
-					return console.log('dnd error: draggble is null')
+    window.addEventListener("load", () => {
+        socket.listen('dnd', function (response) {
+            try {
+                let data = response.data;
+                if (data.path) {
+                    let iframe = document.querySelector(data.path);
+                    let frame = iframe.contentWindow.document || iframe.contentDocument;
+                    data.target = frame.querySelector(data.target);
+                } else {
+                    data.target = document.querySelector(data.target);
+                }
+                if (!data.target)
+                    return console.log('dnd error: draggble is null')
 
-				if (data.dropType == 'cloneable') {
-					data.value[1] = parseTextToHtml(data.value[1]);
-					if (data.hiddenAttribute) {
-						for(const key of Object.keys(data.hiddenAttribute)) {
-							data.value[1].dnd[key] = data.hiddenAttribute[key];
-						}
-					}
-				} else {
-					if (data.value[1])
-						data.value[1] = document.querySelector(data.value[1]);
-					if (!data.value[1])
-						return console.log('dnd error: droppable is null')
-				}
+                if (data.dropType == 'cloneable') {
+                    data.value[1] = parseTextToHtml(data.value[1]);
+                    if (data.hiddenAttribute) {
+                        for (const key of Object.keys(data.hiddenAttribute)) {
+                            data.value[1].dnd[key] = data.hiddenAttribute[key];
+                        }
+                    }
+                } else {
+                    if (data.value[1])
+                        data.value[1] = document.querySelector(data.value[1]);
+                    if (!data.value[1])
+                        return console.log('dnd error: droppable is null')
+                }
 
-				let [position, el] = data.value
-				data.target.insertAdjacentElement(position, el)
-			} catch(error) {
-				console.error(error);
-			}
-		})
-	});
+                let [position, el] = data.value
+                data.target.insertAdjacentElement(position, el)
+            } catch (error) {
+                console.error(error);
+            }
+        })
+    });
 
-	window.addEventListener("dndsuccess", (e) => {
-		let { droppedElCSSPath, draggedEl, position, dropType, path } = e.detail;
-		let data = {
-			target: droppedElCSSPath,
-			method: "insertAdjacentElement",
-			value: [position, draggedEl],
-			dropType
-		}
-		if (dropType === "cloneable") {
-			data.path
-			data.value[1] = draggedEl.outerHTML;
-			data.hiddenAttribute = draggedEl.dnd;
-		} else {
-			data.value[1] = cssPath(draggedEl);
-		}
+    window.addEventListener("dndsuccess", (e) => {
+        let { droppedElCSSPath, draggedEl, position, dropType, path } = e.detail;
+        let data = {
+            target: droppedElCSSPath,
+            method: "insertAdjacentElement",
+            value: [position, draggedEl],
+            dropType
+        }
+        if (dropType === "cloneable") {
+            data.path
+            data.value[1] = draggedEl.outerHTML;
+            data.hiddenAttribute = draggedEl.dnd;
+        } else {
+            data.value[1] = cssPath(draggedEl);
+        }
 
-		message.send({
-			broadcastSender: false,
-			broadcastBrowser: 'once',
-			message: "dnd",
-			data
-		});
-	});
+        socket.send({
+            method: "dnd",
+            broadcastSender: false,
+            broadcastBrowser: 'once',
+            data
+        });
+    });
 
 }
 
