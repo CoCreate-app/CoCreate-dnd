a5776df2913ada6da45de1c427903a71b2ed49d5
frankpagan <frank@cocreate.app>
2024-10-01 13:16:40 -0400

    fix: formating

--- src/util/position.js
+++ src/util/position.js
@@ -1,15 +1,14 @@
 export function setPosition(el, wnd, e, dragType) {
-	let parentX = el.parentElement.offsetLeft
-	let parentY = el.parentElement.offsetTop
-	el.style.position = dragType
-	
-	if (dragType === 'absolute') {
-		el.parentElement.style.position = 'relative'
-		el.style.left = e.pageX - parentX - el.dnd.offsetX + "px";
-		el.style.top = e.pageY - parentY - el.dnd.offsetY + "px";
-	}
-	else if (dragType === 'fixed') {
-		el.style.left = e.x - el.dnd.offsetX + "px";
-		el.style.top = e.y - el.dnd.offsetY + "px";
-	}
+    let parentX = el.parentElement.offsetLeft
+    let parentY = el.parentElement.offsetTop
+    el.style.position = dragType
+
+    if (dragType === 'absolute') {
+        el.parentElement.style.position = 'relative'
+        el.style.left = e.pageX - parentX - el.dnd.offsetX + "px";
+        el.style.top = e.pageY - parentY - el.dnd.offsetY + "px";
+    } else if (dragType === 'fixed') {
+        el.style.left = e.x - el.dnd.offsetX + "px";
+        el.style.top = e.y - el.dnd.offsetY + "px";
+    }
 }
